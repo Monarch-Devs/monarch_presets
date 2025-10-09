94eb6f7b4fbe813e1dd83e5fa060faf1cf6121a9
IlMelons <melonss2002@gmail.com>
2025-10-09 19:14:52 +0200

    Adapted Ambitioneers presets for new IPLs version

--- Ambitioneers/ipl.lua
+++ Ambitioneers/ipl.lua
@@ -1,52 +1,63 @@
 ---@diagnostic disable: exp-in-action, unknown-symbol
 ---@description Ambitioneers IPLs Presets [https://ambitioneers.tebex.io/]
 
----@description IPL
-['mile-high-hotel-1'] = { 'sprr_int_placement_mhc_hotel_floor_1_1_milo_' },
-
-['mile-high-hotel-2'] = { 'sprr_int_placement_mhc_hotel_floor_1_2_milo_' },
-
-['mile-high-hotel-3'] = { 'sprr_int_placement_mhc_hotel_floor_1_3_milo_' },
-
-['mile-high-hotel-4'] = { 'sprr_int_placement_mhc_hotel_floor_1_4_milo_' },
-
-['mile-high-hotel-5'] = { 'sprr_int_placement_mhc_hotel_floor_1_5_milo_' },
-
-['mile-high-hotel-6'] = { 'sprr_int_placement_mhc_hotel_floor_1_6_milo_' },
-
----@description Zones
-['mile-high-hotel-1'] = {
-    coords = vec4(-147.78, -986.99, 181.30, 160.00),
+---@description Dynamic IPLs
+['mile_high_hotel_1'] = {
+    coords = vec3(-147.78, -986.99, 181.30),
     size = vec3(22.0, 60.0, 2.5),
+    rotation = 160.0,
     debug = false,
+    ipl = {
+        'sprr_int_placement_mhc_hotel_floor_1_1_milo_',
+    },
 },
 
-['mile-high-hotel-2'] = {
-    coords = vec4(-147.78, -986.99, 187.70, 160.00),
+['mile_high_hotel_2'] = {
+    coords = vec3(-147.78, -986.99, 187.70),
     size = vec3(22.0, 60.0, 2.5),
+    rotation = 160.0,
     debug = false,
+    ipl = {
+        'sprr_int_placement_mhc_hotel_floor_1_2_milo_',
+    },
 },
 
-['mile-high-hotel-3'] = {
-    coords = vec4(-147.78, -986.99, 193.90, 160.00),
+['mile_high_hotel_3'] = {
+    coords = vec3(-147.78, -986.99, 193.90),
     size = vec3(22.0, 60.0, 2.5),
+    rotation = 160.0,
     debug = false,
+    ipl = {
+        'sprr_int_placement_mhc_hotel_floor_1_3_milo_',
+    },
 },
 
-['mile-high-hotel-4'] = {
-    coords = vec4(-147.78, -986.99, 200.00, 160.00),
+['mile_high_hotel_4'] = {
+    coords = vec3(-147.78, -986.99, 200.00),
     size = vec3(22.0, 60.0, 2.5),
+    rotation = 160.0,
     debug = false,
+    ipl = {
+        'sprr_int_placement_mhc_hotel_floor_1_4_milo_',
+    },
 },
 
-['mile-high-hotel-5'] = {
-    coords = vec4(-147.78, -986.99, 206.40, 160.00),
+['mile_high_hotel_5'] = {
+    coords = vec3(-147.78, -986.99, 206.40),
     size = vec3(22.0, 60.0, 2.5),
+    rotation = 160.0,
     debug = false,
+    ipl = {
+        'sprr_int_placement_mhc_hotel_floor_1_5_milo_',
+    },
 },
 
-['mile-high-hotel-6'] = {
-    coords = vec4(-147.78, -986.99, 212.60, 160.00),
+['mile_high_hotel_6'] = {
+    coords = vec3(-147.78, -986.99, 212.60),
     size = vec3(22.0, 60.0, 2.5),
+    rotation = 160.0,
     debug = false,
+    ipl = {
+        'sprr_int_placement_mhc_hotel_floor_1_6_milo_',
+    },
 },
