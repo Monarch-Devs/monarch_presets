0474213f8286ea9b243fbdcdc7a15f717bb44b9d
IlMelons <melonss2002@gmail.com>
2025-10-10 13:54:42 +0200

    Starting Prompt Entitysets

--- Prompt/ipl.lua
+++ Prompt/ipl.lua
@@ -13,3 +13,9 @@
     ['vending'] = { ipl = 'box55_s_market_stalls_vending', default = true },
     ['wholesale'] = { ipl = 'box55_s_market_stalls_wholesale', default = true },
 },
+
+---@description Entitysets
+[127746] = {},              -- Sandy City Hall
+[124418] = {},              -- Los Santos SAHP
+[123906] = {},              -- Highway SAHP
+[132866] = {},              -- Sandy Motel
