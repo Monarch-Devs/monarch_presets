037ee31b3166424d5adaf4ea15b07c47bc3282fd
IlMelons <melonss2002@gmail.com>
2025-10-11 18:24:44 +0200

    Added Prompt Sandy Cityhall IPL preset

--- Prompt/ipl.lua
+++ Prompt/ipl.lua
@@ -2,6 +2,21 @@
 ---@description Prompt IPLs Presets [https://store.prompt-mods.com/]
 
 ---@description Static IPLs
+['sandy_cityhall'] = {
+    ['fences_closed'] = { ipl = 'p_prompt_sandy_cityhall_fences_backclosed', default = false },
+    ['fences_opened'] = { ipl = 'p_prompt_sandy_cityhall_fences_backopened', default = true },
+    ['leaves'] = { ipl = 'p_prompt_sandy_cityhall_leaves', default = true },
+    ['dinner_tables'] = { ipl = 'p_prompt_sandy_cityhall_tables', default = false },
+    ['dinner_backtables'] = { ipl = 'p_prompt_sandy_cityhall_backtables', default = false },
+    ['coffin_closed'] = { ipl = 'p_prompt_sandy_cityhall_coffin_closed', default = false },
+    ['coffin_opened'] = { ipl = 'p_prompt_sandy_cityhall_coffin_opened', default = false },
+    ['funeral_chairs'] = { ipl = 'p_prompt_sandy_cityhall_funeral_chairs', default = false },
+    ['funeral_picture'] = { ipl = 'p_prompt_sandy_cityhall_funeral_picture', default = false },
+    ['wedding_chairs'] = { ipl = 'p_prompt_sandy_cityhall_wedding_chairs', default = false },
+    ['wedding_spotlight'] = { ipl = 'p_prompt_sandy_cityhall_wedding_spotlight', default = false },
+    ['wedding_venue'] = { ipl = 'p_prompt_sandy_cityhall_wedding_venue', default = false },
+
+},
 ['sandy_market'] = {
     ['arcade'] = { ipl = 'box55_s_market_stalls_arcade', default = true },
     ['boats'] = { ipl = 'box55_s_market_stalls_boats', default = true },
