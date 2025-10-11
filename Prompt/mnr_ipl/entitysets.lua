41a48764755258376f21d11a361a38d6ad377b80
IlMelons <melonss2002@gmail.com>
2025-10-11 23:20:45 +0200

    Reordered Prompt IPLs presets and added Sandy Motel

---
+++ Prompt/mnr_ipl/entitysets.lua
@@ -0,0 +1,54 @@
+---@diagnostic disable: exp-in-action, unknown-symbol
+---@description Prompt Studio Entitysets Presets [https://store.prompt-mods.com/]
+
+-- [MLO] Sandy City Hall
+[127746] = {
+    ['elevator'] = {
+        name = 'static_elevator',
+        enable = true,
+    },
+    ['conference_chairs'] = {
+        name = 'conference_chairs',
+        enable = true,
+    },
+    ['conference_table'] = {
+        name = 'conference_meeting_table',
+        enable = true,
+    },
+    ['voting'] = {
+        name = 'eventroom_voting',
+        enable = true,
+    },
+},
+
+-- [MLO] Los Santos SAHP
+[124418] = {
+    ['chairs'] = {
+        name = 'onlychairs',
+        enable = false,
+    },
+    ['chairs_tables'] = {
+        name = 'chairsandtables',
+        enable = true,
+    },
+},
+
+-- [MLO] Highway SAHP
+[123906] = {
+    ['chairs'] = {
+        name = 'onlychairs',
+        enable = false,
+    },
+    ['chairs_tables'] = {
+        name = 'chairsandtables',
+        enable = true,
+    },
+},
+
+-- [MLO] Sandy Motel
+[132866] = {
+    ['poker'] = {
+        name = 'poker',
+        enable = true,
+    },
+},
--- Prompt/mnr_ipl/static.lua
+++ Prompt/mnr_ipl/static.lua
@@ -1,7 +1,6 @@
 ---@diagnostic disable: exp-in-action, unknown-symbol
----@description Prompt IPLs Presets [https://store.prompt-mods.com/]
+---@description Prompt Studio Static IPLs Presets [https://store.prompt-mods.com/]
 
----@description Static IPLs
 ['sandy_cityhall'] = {
     ['fences_closed'] = { ipl = 'p_prompt_sandy_cityhall_fences_backclosed', default = false },
     ['fences_opened'] = { ipl = 'p_prompt_sandy_cityhall_fences_backopened', default = true },
@@ -16,6 +15,7 @@
     ['wedding_spotlight'] = { ipl = 'p_prompt_sandy_cityhall_wedding_spotlight', default = false },
     ['wedding_venue'] = { ipl = 'p_prompt_sandy_cityhall_wedding_venue', default = false },
 },
+
 ['sandy_market'] = {
     ['arcade'] = { ipl = 'box55_s_market_stalls_arcade', default = true },
     ['boats'] = { ipl = 'box55_s_market_stalls_boats', default = true },
@@ -26,54 +26,4 @@
     ['tattoo'] = { ipl = 'box55_s_market_stalls_tattoo', default = true },
     ['vending'] = { ipl = 'box55_s_market_stalls_vending', default = true },
     ['wholesale'] = { ipl = 'box55_s_market_stalls_wholesale', default = true },
-},
-
----@description Entitysets
--- [MLO] Sandy City Hall
-[127746] = {
-    ['elevator'] = {
-        name = 'static_elevator',
-        enable = true,
-    },
-    ['conference_chairs'] = {
-        name = 'conference_chairs',
-        enable = true,
-    },
-    ['conference_table'] = {
-        name = 'conference_meeting_table',
-        enable = true,
-    },
-    ['voting'] = {
-        name = 'eventroom_voting',
-        enable = true,
-    },
-},
--- [MLO] Los Santos SAHP
-[124418] = {
-    ['chairs'] = {
-        name = 'onlychairs',
-        enable = false,
-    },
-    ['chairs_tables'] = {
-        name = 'chairsandtables',
-        enable = true,
-    },
-},
--- [MLO] Highway SAHP
-[123906] = {
-    ['chairs'] = {
-        name = 'onlychairs',
-        enable = false,
-    },
-    ['chairs_tables'] = {
-        name = 'chairsandtables',
-        enable = true,
-    },
-},
--- [MLO] Sandy Motel
-[132866] = {
-    ['poker'] = {
-        name = 'poker',
-        enable = true,
-    },
 },
