778df7772e6050d605f7323a5e951b14397e3ca5
IlMelons <melonss2002@gmail.com>
2025-10-11 08:47:42 +0200

    Reordering presets

---
+++ TStudio/mnr_ipl/entitysets.lua
@@ -0,0 +1,194 @@
+---@diagnostic disable: exp-in-action, unknown-symbol
+---@description TStudio Entitysets Presets [https://shop.tstudio3d.com/]
+
+-- [MLO] Fleeca
+[188418] = {
+    ['vault'] = {
+        name = 'fleeca_vaultprops',
+        enable = true,
+    },
+},
+[141057] = {
+    ['vault'] = {
+        name = 'fleeca_vaultprops',
+        enable = true,
+    },
+},
+[137985] = {
+    ['vault'] = {
+        name = 'fleeca_vaultprops',
+        enable = true,
+    },
+},
+[165121] = {
+    ['vault'] = {
+        name = 'fleeca_vaultprops',
+        enable = true,
+    },
+},
+[174849]= {
+    ['vault'] = {
+        name = 'fleeca_vaultprops',
+        enable = true,
+    },
+},
+[189698] = {
+    ['vault'] = {
+        name = 'fleeca_vaultprops',
+        enable = true,
+    },
+},
+
+-- [MLO] Pearls Resort
+[284162] = {
+    ['furnished'] = {
+        name = 'villa_l_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_l_props_no',
+        enable = false,
+    },
+},
+[299010]= {
+    ['furnished'] = {
+        name = 'villa_l_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_l_props_no',
+        enable = false,
+    },
+},
+[298498] = {
+    ['furnished'] = {
+        name = 'villa_l_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_l_props_no',
+        enable = false,
+    },
+},
+[300034] = {
+    ['furnished'] = {
+        name = 'villa_s_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_s_props_no',
+        enable = false,
+    },
+},
+[299778] = {
+    ['furnished'] = {
+        name = 'villa_s_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_s_props_no',
+        enable = false,
+    },
+},
+[298754] = {
+    ['furnished'] = {
+        name = 'villa_s_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_s_props_no',
+        enable = false,
+    },
+},
+[298242] = {
+    ['furnished'] = {
+        name = 'villa_s_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_s_props_no',
+        enable = false,
+    },
+},
+[299522] = {
+    ['furnished'] = {
+        name = 'villa_s_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_s_props_no',
+        enable = false,
+    },
+},
+[299266] = {
+    ['furnished'] = {
+        name = 'villa_s_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_s_props_no',
+        enable = false,
+    },
+},
+[301058] = {
+    ['furnished'] = {
+        name = 'villa_s_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_s_props_no',
+        enable = false,
+    },
+},
+[300546] = {
+    ['furnished'] = {
+        name = 'villa_s_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_s_props_no',
+        enable = false,
+    },
+},
+[300290] = {
+    ['furnished'] = {
+        name = 'villa_s_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_s_props_no',
+        enable = false,
+    },
+},
+[300802] = {
+    ['furnished'] = {
+        name = 'villa_s_props_yes',
+        enable = true,
+    },
+    ['unfurnished'] = {
+        name = 'villa_s_props_no',
+        enable = false,
+    },
+},
+[283906] = {
+    ['casino'] = {
+        name = 'casino_table_slots_wheel_yes',
+        enable = true,
+    },
+    ['logos'] = {
+        name = 'enable_3d_logos',
+        enable = true,
+    },
+},
+
+-- [MLO] VHotel Estate
+[304130] = {
+    ['slots'] = {
+        name = 'r16_casino_slots_on',
+        enable = true,
+    },
+    ['pool_tables'] = {
+        name = 'r2_r3_pool_tables_on',
+        enable = true,
+    },
+},
---
+++ TStudio/mnr_ipl/static.lua
@@ -0,0 +1,111 @@
+---@diagnostic disable: exp-in-action, unknown-symbol
+---@description TStudio Static IPLs Presets [https://shop.tstudio3d.com/]
+
+['fleeca'] = {
+    ['fleeca_1'] = {
+        ipl = 'uniqx_flecca_l1_milo_',
+        default = true,
+    },
+    ['fleeca_2'] = {
+        ipl = 'uniqx_flecca_l2_milo_',
+        default = true,
+    },
+    ['fleeca_3'] = {
+        ipl = 'uniqx_flecca_l3_milo_',
+        default = true,
+    },
+    ['fleeca_4'] = {
+        ipl = 'uniqx_flecca_l4_milo_',
+        default = true,
+    },
+    ['fleeca_5'] = {
+        ipl = 'uniqx_flecca_l5_milo_',
+        default = true,
+    },
+    ['fleeca_6'] = {
+        ipl = 'uniqx_flecca_l6_milo_',
+        default = true,
+    },
+},
+['legion_square'] = {
+    ['default'] = {
+        ipl = 'tstudio_legionsquare_ext_amphi_default',
+        default = true,
+    },
+    ['band'] = {
+        ipl = 'tstudio_legionsquare_ext_amphi_band',
+        default = true,
+    },
+    ['dj'] = {
+        ipl = 'tstudio_legionsquare_ext_amphi_dj',
+        default = true,
+    },
+    ['speaker'] = {
+        ipl = 'tstudio_legionsquare_ext_amphi_speaker',
+        default = true,
+    },
+},
+['pearls_resort'] = {
+    ['restaurant'] = {
+        ipl = 'tstudio_jhn_resort_int_pearls_milo_',
+        default = true,
+    },
+    ['villa_large_1'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_l_01_milo_',
+        default = true,
+    },
+    ['villa_large_2'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_l_02_milo_',
+        default = true,
+    },
+    ['villa_large_3'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_l_03_milo_',
+        default = true,
+    },
+    ['villa_small_1'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_s_01_milo_',
+        default = true,
+    },
+    ['villa_small_2'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_s_02_milo_',
+        default = true,
+    },
+    ['villa_small_3'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_s_03_milo_',
+        default = true,
+    },
+    ['villa_small_4'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_s_04_milo_',
+        default = true,
+    },
+    ['villa_small_5'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_s_05_milo_',
+        default = true,
+    },
+    ['villa_small_6'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_s_06_milo_',
+        default = true,
+    },
+    ['villa_small_7'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_s_07_milo_',
+        default = true,
+    },
+    ['villa_small_8'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_s_08_milo_',
+        default = true,
+    },
+    ['villa_small_9'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_s_09_milo_',
+        default = true,
+    },
+    ['villa_small_10'] = {
+        ipl = 'tstudio_jhn_resort_int_villa_s_10_milo_',
+        default = true,
+    },
+},
+['vhotel_estate'] = {
+    ['interior'] = {
+        ipl = 'johanni_vhotel_milo_',
+        default = true,
+    },
+},
